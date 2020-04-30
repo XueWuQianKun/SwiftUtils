9258cafd9fd0efcb8d79cb525947b2877bf5ed3a
zhaoliangyu <zly@shuquxia.com>
2020-04-30 17:40:04 +0800

    测试

---
+++ Swift_Utils/Classes/Manager.swift
@@ -0,0 +1,15 @@
+//
+//  Manager.swift
+//  Swift_Utils_Example
+//
+//  Created by sunshine on 2020/4/30.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class Manager: NSObject {
+    func testManager(content:String) {
+        print(content)
+    }
+}
